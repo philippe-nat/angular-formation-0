15b564af8bbb466bcdc30e8af622a4fbe3b5902c
Philippe LITOU <plito@natsystem.fr>
2020-08-20 11:17:48 +0200

    fin premier composant

--- src/app/entete/entete.component.ts
+++ src/app/entete/entete.component.ts
@@ -7,24 +7,31 @@ import { Component, OnInit } from '@angular/core';
   // styles:[`p {border:solid blue 1px}`]
 })
 export class EnteteComponent implements OnInit {
-  private readonly _aujourdhui:Date;
-  private readonly _aujourdhuiStr:string;
+  private readonly _aujourdhui: Date;
+  private readonly _aujourdhuiStr: string;
+  private _compteur: number;
 
-  get aujourdhui() {return this._aujourdhui;}
-  get aujourdhuiStr() {return this._aujourdhuiStr;}
-  
-  constructor() { 
+  get aujourdhui() { return this._aujourdhui; }
+  get aujourdhuiStr() { return this._aujourdhuiStr; }
+  get compteur() {return this._compteur;}
+  set compteur(val:number) {this._compteur = val;}
+
+  constructor() {
+    this._compteur = 0;
     this._aujourdhui = new Date();
-    const options = {year:'numeric', month:'numeric', day:'numeric'};
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
     this._aujourdhuiStr = this._aujourdhui.toLocaleDateString('fr-FR', options);
   }
 
   ngOnInit(): void {
   }
 
-  public afficheMomentActuel():string {
-    let auj:Date = new Date();
-    let paf;
-    return auj.toLocaleDateString('fr-FR') +  " " + auj.getHours() + ":" + auj.getMinutes();
+  public afficheMomentActuel(): string {
+    let auj: Date = new Date();
+    return auj.toLocaleDateString('fr-FR') + " " + auj.toLocaleTimeString();
   }
+
+  public incrementeCompteur():void {this.compteur++;}
+  public logCompteur():void {console.log("compteur = ", this.compteur);}
+  public logCoucou():void {console.log("coucou");}
 }
